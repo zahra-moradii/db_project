7b585dbfb3e659e7a5373dcc2c18ac70e10da0fd
minazabihi <61754510+minazabihi@users.noreply.github.com>
2022-07-11 17:14:50 +0300

    Update Admin.go

--- Admin/Admin.go
+++ Admin/Admin.go
@@ -24,20 +24,6 @@ func (db *sql.DB) Delete(id int) (int64, error) {
 }
 
 
-func EditProductNum(db *sql.DB, id) error {  
-    query := "SELECT FROM  products(product_cat, product_brand , product_title , product_price ,product_desc ,product_image , product_keywords  ) 
-    VALUES (?, ? , ? , ? , ? , ? , ?)"
-    ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancelfunc()
-    stmt, err := db.PrepareContext(ctx, query)
-    if err != nil {
-    log.Printf("Error %s when preparing SQL statement", err)
-    return err
-    }
-    defer stmt.Close()
-}
-
-
 
 
 
